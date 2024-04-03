d2605a519b0062331427d001005ce1a9fbe7c6ff
Eben-Succes <aebenezer237@gmail.com>
2024-04-03 23:50:16 +0000

    done with GetFrequency

---
+++ GetFrequency.Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using GetFrequency;
+
+namespace GetFrequency.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestGetFrequency()
+        {
+            // Arrange
+            string str = "hello";
+
+            // Act
+            Dictionary<string, int> result = Program.GetFrequency(str);
+
+            // Assert
+            Assert.AreEqual(4, result.Count);
+            Assert.IsTrue(result.ContainsKey("h"));
+            Assert.IsTrue(result.ContainsKey("e"));
+            Assert.IsTrue(result.ContainsKey("l"));
+            Assert.IsTrue(result.ContainsKey("o"));
+            Assert.AreEqual(1, result["h"]);
+            Assert.AreEqual(1, result["e"]);
+            Assert.AreEqual(2, result["l"]);
+            Assert.AreEqual(1, result["o"]);
+        }
+    }
+}
---
+++ GetFrequency/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program {
+    static void Main(){
+
+        Console.Write("Enter a string: ");
+        string s = Console.ReadLine();
+
+        Console.WriteLine("___Frequency of Characters___");
+        foreach (var item in GetFrequency(s))
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+
+
+        Dictionary<string, int> GetFrequency(string s){
+            Dictionary<string,int> dictionary = new Dictionary<string,int>();
+
+            foreach (char ch in s){
+                if (char.IsLetter(ch)){
+                    string key = ch.ToString(); // Convert char to string
+                    if (dictionary.ContainsKey(key)) {
+                        dictionary[key]++;
+                    }
+                    else{
+                        dictionary[key] = 1;
+                    }
+                }
+            }
+
+            return dictionary;
+        }
+    }
+}
