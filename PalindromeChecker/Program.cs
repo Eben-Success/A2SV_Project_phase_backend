c94fec309995a911c0bb4cc350b1bda0354bf7d0
Eben-Succes <aebenezer237@gmail.com>
2024-04-03 23:43:14 +0000

    Added PalindromeChecker

---
+++ PalindromeChecker/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class PalindromeChecker
+{
+    static void Main()
+    {
+
+        Console.WriteLine("____Palindrome Checker____");
+        Console.WriteLine("Enter a string to check if it is a palindrome: ");
+        string str = Console.ReadLine();
+
+        if (isPalindrome(str))
+        {
+            Console.WriteLine("The string is a palindrome");
+        }
+        else
+        {
+            Console.WriteLine("The string is not a palindrome");
+        }
+        
+        static bool isPalindrome(string str)
+        {
+            int left = 0;
+            int right = str.Length - 1;
+
+            while (left < right)
+            {
+                if (str[left] != str[right])
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+
+            }
+
+            return true;
+        }
+    }
+}
